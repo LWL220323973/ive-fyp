64a35955aa349c186bd24d6cc92796d2d1283ad4
Kelvin啊希 <kahei1215.hk@gmail.com>
2025-02-19 17:41:13 +0800

    update template

--- IVE_FYP.bat
+++ IVE_FYP.bat
@@ -4,18 +4,18 @@ if not exist node_modules (
     echo Installing npm dependencies for admin...
     npm install--legacy-peer-deps
 )
-start powershell -NoExit -Command "set PORT=3000; npm start"
+start pwsh -NoExit -Command "$env:PORT=3000; npm start"
 
 REM 啟動Customer前端
 cd ../customer
 if not exist node_modules (
     echo Installing npm dependencies for customer...
-    npm install --legacy-peer-deps
+    npm install
 )
-start powershell -NoExit -Command "set PORT=3001; npm start"
+start pwsh -NoExit -Command "$env:PORT=3001; npm start"
 
 REM 返回根目錄
 cd ../server
 
 REM 啟動後端
-start powershell -NoExit -Command "mvn clean install; mvn spring-boot:run"
+start pwsh -NoExit -Command "mvn clean install; mvn spring-boot:run"
