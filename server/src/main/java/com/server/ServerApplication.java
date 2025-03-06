518ce0ab49fcb2e75d455f9beb7744bdbe9bda40
Kelvin啊希 <kahei1215.hk@gmail.com>
2025-03-06 13:25:50 +0800

    udpate log settings

--- server/src/main/java/com/server/ServerApplication.java
+++ server/src/main/java/com/server/ServerApplication.java
@@ -1,19 +1,17 @@
 package com.server;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.lang.NonNull;
 
 @Slf4j
 @SpringBootApplication
@@ -22,7 +20,12 @@ public class ServerApplication {
     public static void main(String[] args) {
         try {
             String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            File file = new File("log");
+            if (!file.exists()) {
+                file.mkdir();
+            }
             System.setOut(new PrintStream("log/server" + date + ".log"));
+            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
